243a2bdb9850c22e17efc527f7a20a6aa8e6b273
akiho-natsuhara <natsu.050711@gmail.com>
2022-02-19 18:37:15 +0900

    Update user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
     factory :user do
       nickname {Faker::Name.name}
       email {Faker::Internet.unique.email}
-      password = Faker::Internet.password(min_length: 6)
-      password {password}
+      password { "6t" + Faker::Internet.unique.password(min_length: 6) }
       password_confirmation {password}
       last_name { '山田' }
       first_name { '陸太郎' }
