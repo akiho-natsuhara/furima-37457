0565b18940b510245ecdcf163155f2d88944a2e1
akiho-natsuhara <natsu.050711@gmail.com>
2022-02-22 14:46:38 +0900

    itemテストコードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,6 @@
 class Item < ApplicationRecord
 
     belongs_to :user
-    # extend ActiveHash::Associations::ActiveRecordExtensions
-
-    # # belongs_to :user
-
-    # belongs_to :category
-    # belongs_to :cost
-    # belongs_to :prefecture
-    # belongs_to :status
-    # belongs_to :shipping_day
 
 
     has_one_attached :image
@@ -36,7 +27,6 @@ class Item < ApplicationRecord
 
     extend ActiveHash::Associations::ActiveRecordExtensions
 
-    # belongs_to :user
 
     belongs_to :category
     belongs_to :cost
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -122,6 +122,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it "金額は半角数字でなければ登録できない" do
+        @item.price = "１００００"
+        @item.valid?
+        binding.pry
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end  
 end
