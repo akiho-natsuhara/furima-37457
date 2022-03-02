180314d945a24b67788ad4473637a466de300b24
akiho-natsuhara <natsu.050711@gmail.com>
2022-03-02 12:03:35 +0900

    エラー文の日本語化

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -7,11 +7,11 @@ class PurchaseForm
       validates :user_id
       validates :item_id
      
-      validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-      validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+      validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+      validates :prefecture_id, numericality: { other_than: 0,}
       validates :city
       validates :house_number
-      validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
+      validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
      
       validates :token
     end
